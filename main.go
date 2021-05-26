78ba958d1e5a6f95f6e59b93d53016d667f5338a
HAJIME Fukuna <f97one@gmail.com>
2021-05-26 22:41:01 +0900

    stdinからメッセージを受け付けるよう修正。 #1

--- main.go
+++ main.go
@@ -4,9 +4,12 @@ import (
 	"flag"
 	"fmt"
 	"github.com/f97one/LineWorksBotMessenger/utils"
+	"golang.org/x/crypto/ssh/terminal"
+	"io/ioutil"
 	"log"
 	"os"
 	"path/filepath"
+	"syscall"
 )
 
 func main() {
@@ -17,17 +20,26 @@ func main() {
 	var confFilePath string
 	var authKeyPath string
 	var destUsername string
+	var messages string
 	flag.StringVar(&confFilePath, "c", "", "configuration file path")
 	flag.StringVar(&authKeyPath, "k", "", "Authorization Key file path")
 	flag.StringVar(&destUsername, "d", "", "Destination username to speak")
 
-	// しゃべらせるメッセージは名前なし引数にする
-	if len(flag.Args()) == 0 {
-		exitOnEmpty("Messages to speak must not be empty")
-	}
-	msg := flag.Args()[0]
-
 	flag.Parse()
+
+	// しゃべらせるメッセージは名前なし引数にする
+	if args := flag.Args(); len(args) > 0 {
+		messages = args[0]
+	}
+
+	if len(messages) == 0 {
+		if terminal.IsTerminal(int(syscall.Stdin)) {
+			exitOnEmpty("Messages to speak must not be empty")
+		}
+		messages = readFromStdin()
+	} else if messages == "-" {
+		messages = readFromStdin()
+	}
 
 	if len(confFilePath) == 0 {
 		exitOnEmpty("configuration file path must not be empty")
@@ -41,14 +53,14 @@ func main() {
 		exitOnEmpty("Destination username to speak must not be empty")
 	}
 
-	if len(msg) == 0 {
+	if len(messages) == 0 {
 		exitOnEmpty("Messages to speak must not be empty")
 	}
 
 	log.Printf("得られた値 : confFilePath = %s\n", confFilePath)
 	log.Printf("得られた値 : authKeyPath = %s\n", authKeyPath)
 	log.Printf("得られた値 : destUsername = %s\n", destUsername)
-	log.Printf("得られた値 : msg = %s\n", msg)
+	log.Printf("得られた値 : msg = %s\n", messages)
 
 	conf, err := utils.Load(filepath.Clean(confFilePath))
 	if err != nil {
@@ -64,12 +76,20 @@ func main() {
 		log.Fatalln(err)
 	}
 
-	err = sendToUser(accessToken, conf, destUsername, msg)
+	err = sendToUser(accessToken, conf, destUsername, messages)
 	if err != nil {
 		log.Fatalln(err)
 	}
 
 	os.Exit(0)
+}
+
+func readFromStdin() string {
+	msgBytes, err := ioutil.ReadAll(os.Stdin)
+	if err != nil {
+		exitOnEmpty("Could not read messages to speak")
+	}
+	return string(msgBytes)
 }
 
 func exitOnEmpty(msg string) {
